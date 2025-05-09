aada9d371999ee95837c4eb783d8bd630b7e4e85
Shubham Sharma <shubhamku044@gmail.com>
2025-05-09 22:00:22 +0530

    refactor: remove unused authentication files and simplify welcome page layout

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,6 +15,8 @@ class BookFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Book::class;
+
     public function definition(): array
     {
         return [
