bddab8e81595bc2c20ad360be1f5b11ae01ce3e2
Shubham Sharma <shubhamku044@gmail.com>
2025-05-10 10:32:05 +0530

    feat: add migrations for books, sessions, and cache tables; update welcome page to display books in a table

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,6 +1,22 @@
-import { Head   } from '@inertiajs/react';
+import AppearanceToggleDropdown from '@/components/appearance-dropdown';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui';
+import { Head } from '@inertiajs/react';
+import { useEffect, useState } from 'react';
 
 export default function Welcome() {
+    const [books, setbooks] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/v1/books')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Books:', data);
+                setbooks(data.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching books:', error);
+            });
+    }, []);
 
     return (
         <>
@@ -8,9 +24,33 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="">
-                <main className="flex text-white w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
-                    <h1>Hello world</h1>
+            <div className="p-8">
+                <header className="flex items-center justify-end">
+                    <AppearanceToggleDropdown />
+                </header>
+                <main className="w-full dark:text-white">
+                    <div className="mx-auto max-w-3xl">
+                        <Table>
+                            <TableCaption>List of Books</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Title</TableHead>
+                                    <TableHead>Author</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {books.map((book) => {
+                                    const { title, author, id } = book;
+                                    return (
+                                        <TableRow key={id}>
+                                            <TableCell className="font-medium">{title}</TableCell>
+                                            <TableCell>{author}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </div>
                 </main>
             </div>
         </>
